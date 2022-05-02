66ef8180a738e2939fb44bca260548398c0972b0
jose mazariegos <jcmazarriegos@scl.edu.gt>
2022-05-02 17:52:29 -0600

    acabe

---
+++ js/category.js
@@ -0,0 +1,11 @@
+///////Base de datos con el id y el nombre de cada categoría.
+
+let Category = [
+    [4000,	'Laptops'],
+    [4100,	'Mobile phones'],
+    [4200,	'Tablets'],
+    [4300,	'Servers'],
+    [4400,	'PC'],
+    [4500,	'Cables'],
+    [4600,	'Headphones']
+]
